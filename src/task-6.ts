aef24e2cb2f73d0397c4a494fa18ae8d44307a05
Igor_Oros <orosigor1992@gmail.com>
2026-05-05 00:33:58 +0300

    fixed

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,9 +2,9 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-const firstNumber = getFirstElement([1, 2, 3]); // 1
-const firstString = getFirstElement(["a", "b", "c"]); // "a"
-const firstBoolean = getFirstElement([true, false, true]); // true
+const firstNumber = getFirstElement<number>([1, 2, 3]);
+const firstString = getFirstElement<string>(["a", "b", "c"]);
+const firstBoolean = getFirstElement<boolean>([true, false, true]);
 
 console.log(firstNumber); // 1
 console.log(firstString); // "a"
